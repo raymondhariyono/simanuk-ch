f4a508398b7072fd5e98ba1cef179e452c7fb599
Chaidar <chaidarmerak1996@gmail.com>
2025-11-19 20:59:48 +0800

    Menambahkan Model Sarana serta menampilkannya di sarpras_view

--- simanuk/app/Controllers/Peminjam/SarprasController.php
+++ simanuk/app/Controllers/Peminjam/SarprasController.php
@@ -3,24 +3,29 @@
 namespace App\Controllers\Peminjam;
 
 use App\Controllers\BaseController;
-use App\Models\SaranaModel;
+use App\Models\Sarpras\SaranaModel;
+use App\Models\Sarpras\PrasaranaModel;
 
 class SarprasController extends BaseController
 {
    protected $saranaModel;
+   protected $prasaranaModel;
 
    public function __construct()
    {
       $this->saranaModel = new SaranaModel();
+      $this->prasaranaModel = new PrasaranaModel();
    }
 
    public function index()
    {
       $sarana = $this->saranaModel->getSaranaForKatalog();
+      $prasarana = $this->prasaranaModel->getPrasaranaForKatalog();
 
       $data = [
-         'title' => 'Katalog Inventarisasi',
+         'title' => 'Katalog Sarpras',
          'sarana' => $sarana,
+         'prasarana' => $prasarana,
          'showSidebar' => true, // flag untuk sidebar
          'breadcrumbs' => [
             [
@@ -28,11 +33,11 @@ class SarprasController extends BaseController
                'url' => site_url('admin/dashboard')
             ],
             [
-               'name' => 'Katalog Inventarisasi',
+               'name' => 'Sarpras',
             ]
          ]
       ];
 
-      return view('peminjam/katalog_view', $data);
+      return view('peminjam/sarpras_view', $data);
    }
 }
--- simanuk/app/Views/layout/sidebar.php
+++ simanuk/app/Views/layout/sidebar.php
@@ -77,7 +77,7 @@
             <svg class="w-6 h-6 <?= getIconClasses('peminjam/katalog') ?>" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
-            <span>Katalog Barang</span>
+            <span>Katalog Sarpras</span>
          </a>
          <a href="<?= site_url('peminjam/histori-peminjaman') ?>" class="<?= getLinkClasses('peminjam/histori-peminjaman') ?>">
             <svg class="w-6 h-6 <?= getIconClasses('peminjam/histori-peminjaman') ?>" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
